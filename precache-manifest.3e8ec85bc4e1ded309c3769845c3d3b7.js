3784ab18502f1f3362f43ef312a9c1f9613b66f2
Le Anh Quan <quanla2003@gmail.com>
2019-08-21 22:14:49 +0700

    a

---
+++ precache-manifest.3e8ec85bc4e1ded309c3769845c3d3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea43eba3072ab59e97da265a032d4ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afec61b104065a5b2221",
+    "url": "/static/css/main.081297e6.chunk.css"
+  },
+  {
+    "revision": "ae85a07069dfb40d36bb",
+    "url": "/static/js/2.875e0f04.chunk.js"
+  },
+  {
+    "revision": "afec61b104065a5b2221",
+    "url": "/static/js/main.81686dca.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
